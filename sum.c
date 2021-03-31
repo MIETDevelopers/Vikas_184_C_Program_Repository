e43ae3c01ce6354c76755c5476046f199c582f32
vikaskotwal2002 <76261604+vikaskotwal2002@users.noreply.github.com>
2021-03-31 13:21:46 +0530

    addition program

---
+++ sum.c
@@ -0,0 +1,17 @@
+// Author : Vikas Kotwal
+// Creation Date : 18-03-21
+// Purpose : Addition of Two numbers
+#include <stdio.h> // Preprocessive to include standard input and output 
+int main() { // Main Body   
+
+    int num1, num2, sum; // Declaring Variable
+    
+    printf("Enter two integers: "); // taking input 
+    scanf("%d %d", &num1, &num2); // Scaning output
+
+    // calculating sum
+    sum = num1 + num2;    // Logic  
+    
+    printf("%d + %d = %d", num1, num2, sum); // Priniting output
+    return 0;
+}   
