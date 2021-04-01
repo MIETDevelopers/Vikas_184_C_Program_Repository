322d79bdd439665db27fc82eb8cf785a4e66a7fd
vikas kotwal <76261604+vikaskotwal2002@users.noreply.github.com>
2021-04-01 21:22:14 +0530

    C Programs added

---
+++ addition_array.c
@@ -0,0 +1,28 @@
+// Author : Vikas Kotwal
+// Creation Date: 18-03-21
+// Purpose : Addition Of Matrix 2D
+#include <stdio.h> // Pre-Processive to include Standard input and out header files funtions
+int main() // Start of Main body 
+{
+	int a[2][3],b[2][3],c[2][3],i,j; // Declaring Variables 
+	printf("\nENTER VALUES FOR MATRIX A:\n"); // taking Matrix A elemnts
+	for(i=0;i<2;i++) // for loop 
+		for(j=0;j<3;j++) // for loop 
+			scanf("%d",&a[i][j]); // taking input 
+	printf("\nENTER VALUES FOR MATRIX B:\n");  // taking Matrix B elemnts 
+	for(i=0;i<2;i++)// for loop 
+		for(j=0;j<3;j++)// for loop 
+			scanf("%d",&b[i][j]);// taking input 
+	for(i=0;i<2;i++)// for loop 
+		for(j=0;j<3;j++)// for loop 
+			c[i][j]=a[i][j]+b[i][j]; //Logic 
+	printf("\nTHE VALUES OF MATRIX C ARE:\n"); // Prinitng the Matrix C 
+	for(i=0;i<2;i++)
+        {
+		for(j=0;j<3;j++) //for loop 
+			printf("%5d",c[i][j]); // Printing the matrix 
+		printf("\n");
+	}
+	return 0; 
+}
+// End of Body
---
+++ factorial.c
@@ -0,0 +1,23 @@
+// Author : vikas kotwal
+// Creation Date: 20-3-21
+//  Purpose: To Calculate the factorial of given number
+#include <stdio.h>
+int main() {
+    int n, i;
+   float fact = 1;
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+
+    // Error if the user enters a negative integer
+    if (n < 0)
+        printf("Error! Factorial of a negative number doesn't exist.");
+    else {
+        for (i = 1; i <= n; ++i) {
+            fact *= i;
+        }
+        printf("Factorial of %d = %f", n, fact);
+    }
+
+    return 0;
+}
+
